1a6b5da98dfd4a2a3fb2f895fc921972035e2a5c
25LP7668 25LP7668 <25LP7668@Tuvshins-iMac.local>
2025-10-17 09:26:29 +0800

    commit

--- main.js
+++ main.js
@@ -1,4 +1,25 @@
 import fs from "fs";
-const data = fs.readFileSync("data.txt", "utf8");
-const index = data.indexOf("bananas are for monkeys");
-console.log(index);
+
+const words = (file) => {
+  const data = fs.readFileSync(file, "utf8");
+  const index = data.indexOf("bananas are for monkeys");
+  return index;
+};
+console.log(words(`data.txt`));
+
+const ionEnd = (ion, ending) => {
+  const words = fs.readFileSync(ion, "utf8");
+  const ionWords = new RegExp(`\\b[\\w'-]*${ending}\\b`, "gi");
+
+  const allMatchWOrds = words.match(ionWords);
+  return allMatchWOrds;
+};
+console.log(ionEnd(`data.txt`, `ion`));
+const reStart = (re, start) => {
+  const words = fs.readFileSync(re, "utf8");
+  const reWords = new RegExp(`\\b${start}[\\w'-]*\\b`, "gi");
+
+  const allMatchWOrds = words.match(reWords);
+  return allMatchWOrds;
+};
+console.log(reStart(`data.txt`, `re`));
